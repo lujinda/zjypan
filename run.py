43cbbe38cd5fa40336f3e1654915e66f770b7370
Jinda Lu <q8886888@gmail.com>
2015-01-18 13:45:43 +0800

    添加 options

--- run.py
+++ run.py
@@ -2,18 +2,21 @@
 #coding:utf8
 # Author          : tuxpy
 # Email           : q8886888@qq.com
-# Last modified   : 2015-01-18 11:54:55
+# Last modified   : 2015-01-18 13:45:14
 # Filename        : run.py
 # Description     : 
 from tornado import httpserver, ioloop, web
 import logging
 from tornado import options
+from tornado.options import options, define
 
 from app import PanApplication
+
+define('port', type=int, default=1234, help='listen port')
 
 if __name__ == "__main__":
     options.parse_command_line()
     http_server = httpserver.HTTPServer(PanApplication())
-    http_server.listen(1234)
+    http_server.listen(options.port)
     ioloop.IOLoop.instance().start()
 
