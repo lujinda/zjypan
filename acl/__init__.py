69362ed7396409c9a328a0dda2e2c7a53d23ffdd
tuxpy <q8886888@qq.com>
2015-04-11 15:16:37 +0800

    更新

--- acl/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#coding:utf8
-# Author          : tuxpy
-# Email           : q8886888@qq.com
-# Last modified   : 2015-02-03 13:15:34
-# Filename        : acl/__init__.py
-# Description     : 
-from acl import ACL
-
